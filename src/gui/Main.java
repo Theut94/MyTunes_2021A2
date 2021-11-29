c038f6781ec4118123120470b766cecd253d28d3
Theut94 <Mikkeltheut@gmail.com>
2021-11-29 12:17:35 +0100

    MyTunes GUI start

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,7 +1,23 @@
 package gui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MyTunes.fxml"));
+        primaryStage.setTitle("MyTunes");
+        primaryStage.setScene(new Scene(root, 900, 600));
+        primaryStage.show();
+
+    }
     public static void main(String[] args) {
+        launch(args);
         
     }
 }

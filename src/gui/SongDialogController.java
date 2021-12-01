ab238b38686918098ebb32998dd9f09a6674f5a2
anne507x <anne507x@easv365.dk>
2021-12-01 11:24:41 +0100

    Created Song dialog scene

---
+++ src/gui/SongDialogController.java
@@ -0,0 +1,25 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SongDialogController {
+    @FXML
+    private TextField txtTitle;
+    @FXML
+    private TextField txtArtist;
+    @FXML
+    private TextField txtTime;
+    @FXML
+    private TextField txtPath;
+
+    public void ChoosePath(ActionEvent actionEvent) {
+    }
+
+    public void Cancel(ActionEvent actionEvent) {
+    }
+
+    public void Save(ActionEvent actionEvent) {
+    }
+}

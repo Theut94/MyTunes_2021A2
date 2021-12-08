66bea5c3148dc1538e6f8ca61de7e1fcf1032a97
Theut94 <Mikkeltheut@gmail.com>
2021-12-08 10:02:42 +0100

    We can pause and unpause a song.

--- src/bll/util/MusicPlayer.java
+++ src/bll/util/MusicPlayer.java
@@ -3,6 +3,7 @@ package bll.util;
 
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
 /**
  * In this class, we have the methods for playing songs.
@@ -12,6 +13,8 @@ public class MusicPlayer
     private Media media;
     private MediaPlayer mp;
     private boolean isPlaying = false;
+    private Duration timeWhenPaused;
+    private boolean hasBeenPaused;
 
     public MusicPlayer()
     {
@@ -21,18 +24,33 @@ public class MusicPlayer
     {
         media = new Media(file);
         mp = new MediaPlayer(media);
-        mp.play();
+       if(!hasBeenPaused)
+       {mp.play();
+        mp.setAutoPlay(true);
         isPlaying = true;
+       hasBeenPaused = false;}
+      else {
+           mp.setStartTime(timeWhenPaused);
+           mp.play();
+           hasBeenPaused = false;
+           isPlaying = true;
+       }
+
+
     }
 
     public  void stopPlaying()
     {
-        mp.stop();
+        timeWhenPaused= mp.getCurrentTime();
+        mp.pause();
         isPlaying = false;
+        hasBeenPaused = true;
+
     }
 
     public void nextSong()
     {
+        mp.setOnEndOfMedia(null);
     }
 
     public boolean isPlaying()
